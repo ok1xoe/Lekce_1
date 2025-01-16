3b8c17d540cee4117bfbfd2391874a9065eb9d1a
Tomáš Kaplan <tomas.kaplan@gmail.com>
2025-01-16 23:21:57 +0100

    prvni verze

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
+
+    /**
+     * Author: Tomáš Kaplan
+     * Date: 2025-01-16
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        com.engeto.invoice.Invoice invoice1 = new com.engeto.invoice.Invoice();
+        com.engeto.invoice.Invoice invoice2 = new com.engeto.invoice.Invoice();
+
+        invoice1.setInvoiceNumber(20250001);
+        invoice2.setInvoiceNumber(20250002);
+
+//        customerName = "Tomáš Kaplan";
+//        packageWeight = 125;
+//
+//        double totalWeight  = packageWeight + 2;
+        System.out.println("Faktura 1");
+        System.out.println("Číslo faktury: " + invoice1.getInvoiceNumber());
+        System.out.println("Jméno zákazníka: "  + invoice1.getCustomerName());
+        System.out.println("Datum vystavení faktury: " + invoice1.getInvoiceDate());
+        System.out.println("Celková částka: " + invoice1.getTotalAmount());
+
+        System.out.println("Faktura 2");
+        System.out.println("Číslo faktury: " + invoice2.getInvoiceNumber());
+        System.out.println("Jméno zákazníka: "  + invoice2.getCustomerName());
+        System.out.println("Datum vystavení faktury: " + invoice2.getInvoiceDate());
+        System.out.println("Celková částka: " + invoice2.getTotalAmount());
+
+
+        String name = "Karel" + " " + "Novák";
+        System.out.println(name);
     }
+
 }
--- src/com/engato/invoice/Invoice.java
+++ src/com/engato/invoice/Invoice.java
@@ -1,4 +1,60 @@
-package com.engato.invoice;
+package com.engeto.invoice;
+
+/**
+ * Class for operation with invoices
+ * Author: Tomáš Kaplan
+ * Date: 2025-01-16
+ */
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 
 public class Invoice {
+
+    private int invoiceNumber = 20250001;
+    private String customerName;
+    private LocalDate invoiceDate = LocalDate.now();
+    private BigDecimal totalAmount = new BigDecimal("1250.00");
+    private double packageWeight = 25.5;
+
+    public int getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(int invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public LocalDate getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(LocalDate invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public double getPackageWeight() {
+        return packageWeight;
+    }
+
+    public void setPackageWeight(double packageWeight) {
+        this.packageWeight = packageWeight;
+    }
 }
